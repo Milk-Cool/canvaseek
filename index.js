71c58ab5a521004a1b6ffe23411a64f9a562e16f
Milk-Cool <milan-pl@mail.ru>
2024-05-07 02:24:55 +0300

    config page

---
+++ index.js
@@ -0,0 +1,59 @@
+#!/bin/env node
+
+require("./init.js");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 5960;
+
+const j = p => path.join(__dirname, p);
+
+const replace = (text, replacers) => {
+    for(let key in replacers)
+        text = text.replaceAll(`{{${key}}}`, replacers[key]);
+    return text;
+}
+const replaceServeText = (res, path, replacers) => {
+    let text = fs.readFileSync(path, "utf-8");
+    text = replace(text, replacers);
+    res.send(text);
+}
+
+const getConfig = () => JSON.parse(fs.readFileSync(j("data/config.json")));
+const setConfig = config => fs.writeFileSync(j("data/config.json"), JSON.stringify(config));
+
+const checkConfig = () => {
+    const config = getConfig();
+    if(!(
+        "domain" in config
+        && "key" in config
+        && "max" in config
+    ))
+        return false;
+    return true;
+}
+
+app.use(express.urlencoded({ "extended": false }));
+
+app.get("/style.css", (req, res) => res.sendFile(j("public/style.css")));
+
+app.get("/config", (req, res) => {
+    const config = getConfig();
+    replaceServeText(res, j("public/config.html"), {
+        "domain": config.domain ?? "",
+        "key": config.key ?? "",
+        "max": config.max ?? ""
+    });
+});
+app.post("/config", (req, res) => {
+    setConfig({
+        "domain": req.body.domain,
+        "key": req.body.key,
+        "max": req.body.max
+    });
+    res.redirect("/");
+});
+
+app.listen(port, "localhost", () => console.log(`Listening on ${port}!`));
---
+++ init.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const j = p => path.join(__dirname, p);
+
+if(!fs.existsSync(j("data/")))
+    fs.mkdirSync(j`data/`);
+if(!fs.existsSync(j("data/config.json")))
+    fs.writeFileSync(j`data/config.json`, "{}");
