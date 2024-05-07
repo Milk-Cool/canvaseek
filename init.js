4f82138e3c6dc7f095c8ffbe130c6db68ffdfc8b
Milk-Cool <milan-pl@mail.ru>
2024-05-07 03:44:00 +0300

    another small fix

--- init.js
+++ init.js
@@ -4,6 +4,6 @@ const path = require("path");
 const j = p => path.join(__dirname, p);
 
 if(!fs.existsSync(j("data/")))
-    fs.mkdirSync(j`data/`);
+    fs.mkdirSync(j("data/"));
 if(!fs.existsSync(j("data/config.json")))
-    fs.writeFileSync(j`data/config.json`, "{}");
+    fs.writeFileSync(j("data/config.json"), "{}");
